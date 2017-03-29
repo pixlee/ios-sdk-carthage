149c83f9bd020f8f465afda2d3710de4138a7789
Mohamed Rachidi <mo@mohamed-rachidi.local>
2017-03-28 18:03:41 -0700

    intial commit

---
+++ sdk/sdk.h
@@ -0,0 +1,19 @@
+//
+//  sdk.h
+//  sdk
+//
+//  Created by Mohamed Rachidi on 3/28/17.
+//  Copyright © 2017 Pixlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdk.
+FOUNDATION_EXPORT double sdkVersionNumber;
+
+//! Project version string for sdk.
+FOUNDATION_EXPORT const unsigned char sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk/PublicHeader.h>
+
+
