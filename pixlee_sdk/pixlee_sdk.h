5e24e7ada388e49ad44a5aae3dcdd442d7b3ca4a
Mohamed Rachidi <mo@mohamed-rachidi.local>
2017-03-29 14:34:15 -0700

    reset

---
+++ pixlee_sdk/pixlee_sdk.h
@@ -0,0 +1,30 @@
+//
+//  pixlee_sdk.h
+//  pixlee_sdk
+//
+//  Created by Mohamed Rachidi on 3/29/17.
+//  Copyright © 2017 Pixlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PXLPhoto.h"
+#import "PXLClient.h"
+#import "PXLProductCollectionViewCell.h"
+#import "PXLAlbumSortOptions.h"
+#import "PXLAlbumFilterOptions.h"
+#import "PXLPhotoDetailViewController.h"
+#import "PXLProduct.h"
+#import "PXLAlbum.h"
+#import "PXLAlbumViewController.h"
+#import "PXLPhotoCollectionViewCell.h"
+
+
+//! Project version number for pixlee_sdk.
+FOUNDATION_EXPORT double pixlee_sdkVersionNumber;
+
+//! Project version string for pixlee_sdk.
+FOUNDATION_EXPORT const unsigned char pixlee_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pixlee_sdk/PublicHeader.h>
+
+
