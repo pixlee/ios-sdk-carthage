0b93e8fee16123f77bec7a6b4a326c871d80d38a
mo <rachidi29@gmail.com>
2019-05-14 11:55:27 -0700

    adding sku endpoint

---
+++ Example/pixlee-ios-sdk/PXLSkuAlbumViewController.h
@@ -0,0 +1,19 @@
+//
+//  PXLViewController.h
+//  pixlee-ios-sdk
+//
+//  Created by Tim Shi on 04/30/2015.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+#import "PXLAlbumViewController.h"
+#import <pixlee_sdk/PXLProduct.h>
+
+
+@interface PXLExampleAlbumViewController : PXLAlbumViewController
+@property (nonatomic, strong) PXLPhoto *photo;
+
+@end
---
+++ Example/pixlee-ios-sdk/PXLSkuAlbumViewController.m
@@ -0,0 +1,59 @@
+//
+//  PXLViewController.m
+//  pixlee-ios-sdk
+//
+//  Created by Tim Shi on 04/30/2015.
+//
+
+#import "PXLExampleAlbumViewController.h"\
+
+#import <pixlee_sdk/PXLAlbum.h>
+#import <pixlee_sdk/PXLPhoto.h>
+#import <pixlee_sdk/PXLAlbumFilterOptions.h>
+#import <pixlee_sdk/PXLAlbumSortOptions.h>
+#import <pixlee_sdk/PXLClient.h>
+
+@interface PXLExampleAlbumViewController ()
+
+@end
+
+@implementation PXLExampleAlbumViewController
+
+#warning Replace with your sku identifier.
+static NSString * const PXLSkuAlbumIdentifier = @"2";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Create a new album instance.
+    PXLAlbum *album = [PXLAlbum albumWithSkuIdentifier:PXLSkuAlbumIdentifier];
+    
+    // Create and set filter options on the album.
+    PXLAlbumFilterOptions *filterOptions = [PXLAlbumFilterOptions new];
+    filterOptions.submittedDateStart = [[NSDate date] dateByAddingTimeInterval:-60 * 60 * 24 * 20]; // submitted within the last 20 days
+    album.filterOptions = filterOptions;
+    
+    // Create and set sort options on the album.
+    PXLAlbumSortOptions *sortOptions = [PXLAlbumSortOptions new];
+    sortOptions.sortType = PXLAlbumSortTypePhotoRank;
+    album.sortOptions = sortOptions;
+    album.perPage = 100;
+    
+    // Set the album to load.
+    self.album = album;
+    // Example of loading a photo with an identifier
+    [PXLPhoto getPhotoWithId:@"187542438" callback:^(PXLPhoto *photo, NSError *error) {
+        NSLog(@"%@", photo.cdnOriginalUrl);
+        NSLog(@"%@", photo.cdnLargeUrl);
+        NSLog(@"%@", photo.cdnMediumUrl);
+        NSLog(@"%@", photo.cdnSmallUrl);
+        NSLog(@"%@",error);
+    }];
+    
+
+
+    
+    [self loadNextPageOfPhotos];
+}
+
+@end
