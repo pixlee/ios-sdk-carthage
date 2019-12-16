247725a803cbbf08863d59547b5f64a774ee403b
Csaba Toth <csaba@bitraptors.com>
2019-12-16 22:06:32 +0100

    readded missing files (#14)

---
+++ pixlee_sdk/pixlee_sdk.h
@@ -0,0 +1,30 @@
+//
+//  pixlee_sdk.h
+//  pixlee_sdk
+//
+//  Created by Mohamed Rachidi on 3/29/17.
+//  Copyright © 2017 Pixlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+
+//! Project version number for pixlee_sdk.
+FOUNDATION_EXPORT double pixlee_sdkVersionNumber;
+
+//! Project version string for pixlee_sdk.
+FOUNDATION_EXPORT const unsigned char pixlee_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pixlee_sdk/PublicHeader.h>
+
+
+#import <pixlee_sdk/PXLPhoto.h>
+#import <pixlee_sdk/PXLClient.h>
+#import <pixlee_sdk/PXLAlbumSortOptions.h>
+#import <pixlee_sdk/PXLAlbumFilterOptions.h>
+#import <pixlee_sdk/PXLProduct.h>
+#import <pixlee_sdk/PXLAlbum.h>
+
+
